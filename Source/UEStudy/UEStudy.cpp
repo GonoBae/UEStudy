4e92a25c37c4f74ea89ac861af26e74623e5c645
GonoBae <qldrhqorhsh@naver.com>
2023-07-06 01:38:53 +0900

    Settings

---
+++ Source/UEStudy/UEStudy.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEStudy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEStudy, "UEStudy" );
+ 
